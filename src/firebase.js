207359fea819773d0c019a685695d18bcaebc2db
Tiffany Trinh <acetiff@mit.edu>
2020-12-18 17:47:25 -0800

    localhost and heroku setup

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase';
+
+const app = initializeApp( {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+});
+
+export const db = app.database();
+export const namesRef = db.ref('names');
+
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-analytics.js"></script>
---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueCookie from 'vue-cookie'
+import App from './App.vue'
+import router from './router'
+import Firebase from 'firebase'
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(Firebase);
+Vue.use(firestorePlugin);
+Vue.use(VueCookie);
+
+export const eventBus = new Vue();
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+Firebase.initializeApp(firebaseConfig); 
+
+Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+Firebase.analytics();
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
